2cc2f2e822e28c150fe645a64667bc12a791f05e
github username <github email address>
2019-02-14 12:55:10 +0200

    basic crud

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 // const useless = require('nodejs-class-package');
 // const _ = require('lodash');
 
-useless();
+// useless();
 
-const newArr = _.compact([1,0,false, '', 2]);
+// const newArr = _.compact([1,0,false, '', 2]);
 
-console.log(newArr);
+// console.log(newArr);
+
+
+const fs = require('fs');
+
+let content = fs.readFile('./read.js', "utf8", (err, data) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+
+console.log('after');
+
+
+//delete functionality
+// try {
+//     fs.unlinkSync('./tobedeleted.txt');
+// } catch (e){
+//     console.log(e)
+// } finally {
+//     console.log('done sync delete');
+// }
+
+// fs.unlink("./tobedeleted.txt", err => {
+//     if(err){
+//     console.log(err);
+//     } else {
+//     console.log('done sync');
+//     }
+// })
+
+// console.log('do next step after delete');
