b7e62806d57b10127074015306ace5a7debe16b2
github username <github email address>
2019-02-14 11:43:32 +0200

    install dependencies

---
+++ app.js
@@ -0,0 +1,8 @@
+// const useless = require('nodejs-class-package');
+// const _ = require('lodash');
+
+useless();
+
+const newArr = _.compact([1,0,false, '', 2]);
+
+console.log(newArr);
---
+++ node_modules/nodejs-class-package/useless.js
@@ -0,0 +1,5 @@
+const useless = () => {
+    console.log("Learning is key!")
+}
+
+module.exports = useless;
